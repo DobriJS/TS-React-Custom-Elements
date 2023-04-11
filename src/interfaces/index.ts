d4edfde04ffd2d39b6f2d8000b524b6912d8e29a
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-11 17:50:55 +0300

    create interface folder

---
+++ src/interfaces/index.ts
@@ -0,0 +1,18 @@
+export interface Item {
+    id: string;
+    label: string;
+    content: string;
+}
+
+export interface Items {
+    items: Item[];
+}
+
+export interface DropdownOption {
+    label: string;
+    value: string;
+}
+
+export interface DropdownOptions {
+    options: DropdownOption[];
+}
