3ec8d260ac64cc5f4a0b013184ef5c3465de1fff
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-17 23:44:49 +0300

    check click location

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Panel from './Panel';
 import { GoChevronDown } from 'react-icons/go';
 import { DropdownOption, DropdownOptions } from '../interfaces';
 
 const Dropdown: React.FC<DropdownOptions> = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handler = (event: MouseEvent) => {
+      if (divEl.current && !divEl.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('click', handler, true);
+    return () => document.removeEventListener('click', handler, true);
+  }, []);
 
   const handleClick = () => setIsOpen(!isOpen);
 
@@ -26,7 +37,7 @@ const Dropdown: React.FC<DropdownOptions> = ({ options, value, onChange }) => {
   });
 
   return (
-    <div className='w-48 relative'>
+    <div ref={divEl} className='w-48 relative'>
       <Panel className='flex justify-between items-center cursor-pointer' onClick={handleClick}>
         {value?.label || 'Select...'}
         <GoChevronDown className='text-lg' />
