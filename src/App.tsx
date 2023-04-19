439196eb561a2bd4b77341022918c1ef9bfe364d
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-19 20:34:46 +0300

    render pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import AccordionPage from './pages/AccordionPage';
+import Sidebar from './components/Sidebar';
+import DropdownPage from './pages/DropdownPage';
+import Route from './components/Route';
+
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Sidebar />
+      <div>
+        <Route path='/accordion'>
+          <AccordionPage />
+        </Route>
+        <Route path='/'>
+          <DropdownPage />
+        </Route>
+      </div>
+    </div>
+  );
 };
 
 export default App;
