a1adc0131c318534098a04517b73c6a3d39e419e
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-25 22:48:22 +0300

    add start, add, stop, clear and remove functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
+import { useState } from 'react';
 import Dashboard from './pages/Dashboard';
+import { ErrorLoggerContext, Message } from './context/logger-context';
 
 const App: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isRunning, setIsRunning] = useState<boolean>(true);
+
+  const addMessage = (message: Message) => {
+    setMessages((oldMessages: Message[]) => {
+      const copy = [...oldMessages];
+      copy.unshift(message);
+      return copy;
+    });
+  };
+
+  const stopMessage = () => setIsRunning(false);
+  const startMessage = () => setIsRunning(true);
+  const clearMessage = () => setMessages([]);
+
+  const removeMessage = (message: Message) => {
+    setMessages((oldMessages: Message[]) => {
+      const copy = [...oldMessages];
+      const finalMessages = copy.filter((item: Message) => {
+        return !(item.priority === message.priority && item.message === message.message);
+      });
+      return finalMessages;
+    });
+  };
+
   return (
-    <div>
+    <ErrorLoggerContext.Provider
+      value={{
+        messages,
+        isRunning,
+        addMessage,
+        removeMessage,
+        stopMessage,
+        startMessage,
+        clearMessage
+      }}
+    >
       <Dashboard />
-    </div>
+    </ErrorLoggerContext.Provider>
   );
 };
 
