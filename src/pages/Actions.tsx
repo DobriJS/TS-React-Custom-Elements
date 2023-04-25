291fd0adb204643cd5cf750ac96d7ab278ea5172
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-25 22:56:20 +0300

    add actions page

---
+++ src/pages/Actions.tsx
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import { ErrorLoggerContext } from '../context/logger-context';
+
+const Actions: React.FC = () => {
+  return <div className='container mx-auto flex justify-center'></div>;
+};
+
+export default Actions;
