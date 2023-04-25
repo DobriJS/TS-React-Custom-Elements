64dc833909fdff2df1c188331209cca8f96ecca2
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-25 23:00:22 +0300

    render pages

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,15 @@
+import Header from './Header';
+import Actions from './Actions';
+import TileManager from '../components/tiles/TileManager';
+
 const Dashboard: React.FC = () => {
-  return <div className='container bg-gray-200 h-screen mx-auto'></div>;
+  return (
+    <div className='container bg-gray-200 h-screen mx-auto'>
+      <Header />
+      <Actions />
+      <TileManager />
+    </div>
+  );
 };
 
 export default Dashboard;
