2e2c43a000c173ba433e25526467c77dd71ff51b
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-25 23:00:01 +0300

    add buttons with basic style

--- src/pages/Actions.tsx
+++ src/pages/Actions.tsx
@@ -2,7 +2,33 @@ import { useContext } from 'react';
 import { ErrorLoggerContext } from '../context/logger-context';
 
 const Actions: React.FC = () => {
-  return <div className='container mx-auto flex justify-center'></div>;
+  const { startMessage, stopMessage, isRunning, clearMessage } = useContext(ErrorLoggerContext);
+
+  return (
+    <div className='container mx-auto flex justify-center'>
+      {isRunning ? (
+        <button
+          onClick={() => stopMessage()}
+          className='bg-blue-800 rounded-md text-gray-200 w-24 m-2'
+        >
+          Stop
+        </button>
+      ) : (
+        <button
+          onClick={() => startMessage()}
+          className='bg-blue-800 rounded-md text-gray-200 w-24 m-2'
+        >
+          Start
+        </button>
+      )}
+      <button
+        onClick={() => clearMessage()}
+        className='bg-blue-800 rounded-md text-gray-200 w-24 m-2'
+      >
+        Clear
+      </button>
+    </div>
+  );
 };
 
 export default Actions;
