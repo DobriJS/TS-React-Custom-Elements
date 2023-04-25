cdd5d54c8bfb58915b0f8505eeaab92abe959e9a
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-25 22:56:44 +0300

    add Header page

---
+++ src/pages/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <div className='container mx-auto'>
+      <h3 className='text-2xl m-4 p-4'> Log Message Generator</h3>
+    </div>
+  );
+};
+
+export default Header;
