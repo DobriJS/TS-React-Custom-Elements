bfe403c58591611aa7c67390cce1e8a21ccdcd79
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-26 11:15:02 +0300

    center message text

--- src/components/tiles/TileListItem.tsx
+++ src/components/tiles/TileListItem.tsx
@@ -3,7 +3,7 @@ import { TileListItemProps } from '../../types/TileListItem';
 const TileListItem: React.FC<TileListItemProps> = ({ message, removeMessage }) => {
   return (
     <>
-      <span>{message.message}</span>
+      <div className='text-center'>{message.message}</div>
       <button
         className='rounded-md mt-2 mb-1 text-center bg-white px-4 w-full'
         data-testid='remove'
