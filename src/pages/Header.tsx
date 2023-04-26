12ab835dcb32ed9b53fded93d9a337994a976863
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-04-26 10:58:25 +0300

    center title text

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,7 +1,7 @@
 const Header: React.FC = () => {
   return (
     <div className='container mx-auto'>
-      <h3 className='text-2xl m-4 p-4'> Log Message Generator</h3>
+      <h3 className='text-center text-2xl m-4 p-4'> Log Message Generator</h3>
     </div>
   );
 };
